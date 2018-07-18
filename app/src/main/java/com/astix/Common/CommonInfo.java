cecb5b6843f1db20d2adb84fc8528ed171c5ed4a
SUNIL-PC <astixset2@gmail.com>
2018-07-18 19:13:49 +0530

    Initial commit

---
+++ app/src/main/java/com/astix/Common/CommonInfo.java
@@ -0,0 +1,224 @@
+package com.astix.Common;
+
+import android.net.Uri;
+
+import java.io.File;
+
+public class CommonInfo
+{
+
+
+	// Its for Live Path on 194 Server
+
+/*
+
+	public static int flgAllRoutesData=1;
+	public static File imageF_savedInstance=null;
+	public static String imageName_savedInstance=null;
+	public static String clickedTagPhoto_savedInstance=null;
+	public static Uri uriSavedImage_savedInstance=null;
+
+	public static String imei="";
+	public static String SalesQuoteId="BLANK";
+	public static String quatationFlag="";
+	public static String fileContent="";
+	public static String prcID="NULL";
+
+	public static String newQuottionID="NULL";
+	public static String globalValueOfPaymentStage="0"+"_"+"0"+"_"+"0";
+
+
+
+	public static String WebServicePath="http://103.20.212.194/WebServiceAndroidGeorgiaSFALive/Service.asmx";
+	public static String VersionDownloadPath="http://103.20.212.194/downloads/";
+	public static String VersionDownloadAPKName="GeorgiaSFA.apk";
+
+	public static String DATABASE_NAME = "DbGeorgiaSFAApp";
+
+	public static int AnyVisit = 0;
+
+	public static int DATABASE_VERSIONID = 2;      // put this field value based on value in table on the server
+	public static String AppVersionID = "1.0";   // put this field value based on value in table on the server
+	public static int Application_TypeID = 2; //1=Parag Store Mapping,2=Parag SFA Indirect,3=Parag SFA Direct
+
+	public static String OrderSyncPath="http://103.20.212.194/ReadXML_GeorgiaSFALive/DefaultSFA.aspx";
+	public static String ImageSyncPath="http://103.20.212.194/ReadXML_GeorgiaImagesLive/Default.aspx";
+
+	public static String OrderTextSyncPath="http://103.20.212.194/ReadTxtFileForGeorgiaSFALive/default.aspx";
+	public static String OrderSyncPathDistributorMap="http://103.20.212.194/ReadXML_GeorgiaSFALive/DefaultSODistributorMapping.aspx";
+
+	public static String InvoiceSyncPath="http://103.20.212.194/ReadXML_GeorgiaInvoiceLive/Default.aspx";
+
+	public static String DistributorSyncPath="http://103.20.212.194/ReadXML_GeorgiaSFADistributionLive/Default.aspx";
+
+	public static String OrderXMLFolder="GeorgiaSFAXml";
+	public static String ImagesFolder="GeorgiaSFAImages";
+	public static String TextFileFolder="GeorgiaTextFile";
+	public static String InvoiceXMLFolder="GeorgiaInvoiceXml";
+	public static String FinalLatLngJsonFile="GeorgiaSFAFinalLatLngJson";
+
+	public static final String DistributorMapXMLFolder="GeorgiaDistributorMapXML";
+	public static final String DistributorStockXMLFolder="GeorgiaDistributorStockXML";
+	public static final String DistributorCheckInXMLFolder="GeorgiaDistributorCheckInXML";
+
+	public static String AppLatLngJsonFile="GeorgiaSFALatLngJson";
+
+	public static int DistanceRange=3000;
+	public static String SalesPersonTodaysTargetMsg="";
+	public static final String Preference="GeorgiaPrefrence";
+	public static final String AttandancePreference="GeorgiaAttandancePreference";
+
+
+	public static int CoverageAreaNodeID=0;
+	public static int CoverageAreaNodeType=0;
+	public static int SalesmanNodeId=0;
+	public static int DayStartClick=0;
+	public static String ImagesFolderServer="GeorgiaSFAImagesServer";
+
+	public static int SalesmanNodeType=0;
+	public static int flgDataScope=0;
+	public static int FlgDSRSO=0;
+*/
+
+
+	// Its for Test Path on 194 Server
+
+
+/*	public static int flgAllRoutesData=1;
+	public static File imageF_savedInstance=null;
+	public static String imageName_savedInstance=null;
+	public static String clickedTagPhoto_savedInstance=null;
+	public static Uri uriSavedImage_savedInstance=null;
+
+	public static String imei="";
+	public static String SalesQuoteId="BLANK";
+	public static String quatationFlag="";
+	public static String fileContent="";
+	public static String prcID="NULL";
+
+	public static String newQuottionID="NULL";
+	public static String globalValueOfPaymentStage="0"+"_"+"0"+"_"+"0";
+
+
+
+	public static String WebServicePath="http://103.20.212.194/WebServiceAndroidGeorgiaSFATest/Service.asmx";
+	public static String VersionDownloadPath="http://103.20.212.194/downloads/";
+	public static String VersionDownloadAPKName="GeorgiaSFATest.apk";
+
+	public static String DATABASE_NAME = "DbGeorgiaSFAApp";
+
+	public static int AnyVisit = 0;
+
+	public static int DATABASE_VERSIONID = 2;      // put this field value based on value in table on the server
+	public static String AppVersionID = "1.0";   // put this field value based on value in table on the server
+	public static int Application_TypeID = 2; //1=Parag Store Mapping,2=Parag SFA Indirect,3=Parag SFA Direct
+
+	public static String OrderSyncPath="http://103.20.212.194/ReadXML_GeorgiaSFATest/DefaultSFA.aspx";
+	public static String ImageSyncPath="http://103.20.212.194/ReadXML_GeorgiaImagesTest/Default.aspx";
+
+	public static String OrderTextSyncPath="http://103.20.212.194/ReadTxtFileForGeorgiaSFATest/default.aspx";
+	public static String OrderSyncPathDistributorMap="http://103.20.212.194/ReadXML_GeorgiaSFATest/DefaultSODistributorMapping.aspx";
+
+	public static String InvoiceSyncPath="http://103.20.212.194/ReadXML_GeorgiaInvoiceTest/Default.aspx";
+
+	public static String DistributorSyncPath="http://103.20.212.194/ReadXML_GeorgiaSFADistributionTest/Default.aspx";
+
+	public static String OrderXMLFolder="GeorgiaSFAXml";
+	public static String ImagesFolder="GeorgiaSFAImages";
+	public static String TextFileFolder="GeorgiaTextFile";
+	public static String InvoiceXMLFolder="GeorgiaInvoiceXml";
+	public static String FinalLatLngJsonFile="GeorgiaSFAFinalLatLngJson";
+
+	public static final String DistributorMapXMLFolder="GeorgiaDistributorMapXML";
+	public static final String DistributorStockXMLFolder="GeorgiaDistributorStockXML";
+	public static final String DistributorCheckInXMLFolder="GeorgiaDistributorCheckInXML";
+
+	public static String AppLatLngJsonFile="GeorgiaSFALatLngJson";
+
+	public static int DistanceRange=3000;
+	public static String SalesPersonTodaysTargetMsg="";
+	public static final String Preference="GeorgiaPrefrence";
+	public static final String AttandancePreference="GeorgiaAttandancePreference";
+
+
+	public static int CoverageAreaNodeID=0;
+	public static int CoverageAreaNodeType=0;
+	public static int SalesmanNodeId=0;
+	public static int DayStartClick=0;
+	public static String ImagesFolderServer="GeorgiaSFAImagesServer";
+
+	public static int SalesmanNodeType=0;
+	public static int flgDataScope=0;
+	public static int FlgDSRSO=0;*/
+
+// Its for Development Path on 194 Server
+
+
+	public static int flgAllRoutesData=1;
+	public static File imageF_savedInstance=null;
+	public static String imageName_savedInstance=null;
+	public static String clickedTagPhoto_savedInstance=null;
+	public static Uri uriSavedImage_savedInstance=null;
+
+	public static String imei="";
+	public static String SalesQuoteId="BLANK";
+	public static String quatationFlag="";
+	public static String fileContent="";
+	public static String prcID="NULL";
+
+	public static String newQuottionID="NULL";
+	public static String globalValueOfPaymentStage="0"+"_"+"0"+"_"+"0";
+
+
+
+	public static String WebServicePath="http://103.20.212.194/WebServiceAndroidGeorgiaSFADevelopment/Service.asmx";
+	public static String VersionDownloadPath="http://103.20.212.194/downloads/";
+	public static String VersionDownloadAPKName="GeorgiaSFADevelopment.apk";
+
+	public static String DATABASE_NAME = "DbGeorgiaSFAApp";
+
+	public static int AnyVisit = 0;
+
+	public static int DATABASE_VERSIONID = 2;      // put this field value based on value in table on the server
+	public static String AppVersionID = "1.0";   // put this field value based on value in table on the server
+	public static int Application_TypeID = 2; //1=Parag Store Mapping,2=Parag SFA Indirect,3=Parag SFA Direct
+
+	public static String OrderSyncPath="http://103.20.212.194/ReadXML_GeorgiaSFADevelopment/DefaultSFA.aspx";
+	public static String ImageSyncPath="http://103.20.212.194/ReadXML_GeorgiaImagesDevelopment/Default.aspx";
+
+	public static String OrderTextSyncPath="http://103.20.212.194/ReadTxtFileForGeorgiaSFADevelopment/default.aspx";
+	public static String OrderSyncPathDistributorMap="http://103.20.212.194/ReadXML_GeorgiaSFADevelopment/DefaultSODistributorMapping.aspx";
+
+	public static String InvoiceSyncPath="http://103.20.212.194/ReadXML_GeorgiaInvoiceDevelopment/Default.aspx";
+
+	public static String DistributorSyncPath="http://103.20.212.194/ReadXML_GeorgiaSFADistributionDevelopment/Default.aspx";
+
+	public static String OrderXMLFolder="GeorgiaSFAXml";
+	public static String ImagesFolder="GeorgiaSFAImages";
+	public static String TextFileFolder="GeorgiaTextFile";
+	public static String InvoiceXMLFolder="GeorgiaInvoiceXml";
+	public static String FinalLatLngJsonFile="GeorgiaSFAFinalLatLngJson";
+
+	public static final String DistributorMapXMLFolder="GeorgiaDistributorMapXML";
+	public static final String DistributorStockXMLFolder="GeorgiaDistributorStockXML";
+	public static final String DistributorCheckInXMLFolder="GeorgiaDistributorCheckInXML";
+
+	public static String AppLatLngJsonFile="GeorgiaSFALatLngJson";
+
+	public static int DistanceRange=3000;
+	public static String SalesPersonTodaysTargetMsg="";
+	public static final String Preference="GeorgiaPrefrence";
+	public static final String AttandancePreference="GeorgiaAttandancePreference";
+
+
+	public static int CoverageAreaNodeID=0;
+	public static int CoverageAreaNodeType=0;
+	public static int SalesmanNodeId=0;
+	public static int DayStartClick=0;
+	public static String ImagesFolderServer="GeorgiaSFAImagesServer";
+
+	public static int SalesmanNodeType=0;
+	public static int flgDataScope=0;
+	public static int FlgDSRSO=0;
+
+}
